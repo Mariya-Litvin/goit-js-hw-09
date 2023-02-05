8205f1a466a6d09dc7bda4a4d061d87a69af51ef
Mariya-Litvin <111592916+Mariya-Litvin@users.noreply.github.com>
2023-02-05 02:03:24 +0200

    task 1-3

---
+++ src/js/02-timer.js
@@ -0,0 +1,73 @@
+// Завдання 2 - таймер зворотного відліку
+
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const startBtn = document.querySelector('button[data-start]');
+const dataDays = document.querySelector('span[data-days]');
+const dataHours = document.querySelector('span[data-hours]');
+const dataMinutes = document.querySelector('span[data-minutes]');
+const dataSeconds = document.querySelector('span[data-seconds]');
+const dataTime = document.querySelector('input#datetime-picker');
+let timerId = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      startBtn.disabled = true;
+      Notiflix.Notify.failure('Please choose a date in the future');
+    } else {
+      startBtn.disabled = false;
+    }
+  },
+};
+
+flatpickr(dataTime, options);
+
+startBtn.addEventListener('click', onBtnStart);
+
+function onBtnStart(event) {
+  timerId = setInterval(() => {
+    let countTime = new Date(dataTime.value) - Date.now();
+    let timeObject = convertMs(countTime);
+    if (countTime >= 0) {
+      dataDays.textContent = addLeadingZero(timeObject.days);
+      dataHours.textContent = addLeadingZero(timeObject.hours);
+      dataMinutes.textContent = addLeadingZero(timeObject.minutes);
+      dataSeconds.textContent = addLeadingZero(timeObject.seconds);
+    } else {
+      Notiflix.Notify.success('Congratulation!');
+      clearInterval(timerId);
+    }
+  }, 1000);
+}
+
+/-----------------------------------------------------/;
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, 0);
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+// Завдання 3 - генератор промісів
+
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', promiseFunction);
+
+function promiseFunction(event) {
+  event.preventDefault();
+
+  let firstDelay = Number(form.elements.delay.value);
+  let stepDelay = Number(form.elements.step.value);
+  let amount = Number(form.elements.amount.value);
+
+  for (let i = 0; i < amount; i += 1) {
+    createPromise(i + 1, firstDelay + i * stepDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
